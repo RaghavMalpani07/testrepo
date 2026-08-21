beea7cfca2a3cb6f9fd2bc0b2c5afb41aef97eca
Raghav Malpani <raghavmalpani06@gmail.com>
2026-08-21 21:44:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+# HTML hypertext markup lang
+# hypertext= pieces of text/links which can link to other documents
+# markup like bold "" underline in htms markup is done by HTML tags
+# HTML tags = heading paragraph anchor lists
+
+
+# heading tag = <h1>Hello world</h1> entire thing = element
+# total 6 tags h1 to h6
+
+# paragraph element <p>Hello world</p>
+# void elements = no contents just tags
+# <hr> and <br>
+# hr creates a horizontal line and br breaks a paragraph
+
+# HTML boilerplate(structure of html file)
+
+# list elements <ul></ul> and <ol></ol>
+#<ul>
+#   <li> </li>
+#</ul>
+
+# HTML attribute= <tag attribute=value>Content</tag>
+# anchor tag = <a href=https://www.youtube.com/>This is a link</a>
+
+# image tag <img src='url' />  (void element)
+#
+# import requests
+#
+# #
+# # response=requests.get('https://appbrewery.github.io/news.ycombinator.com/')
+# # yc_webpage=response.text
+# # soup=BeautifulSoup(yc_webpage,'html.parser')
+# #
+# # article=soup.find(name='a', class_="storylink")
+# # article_text= article.text
+# # article_link=article.get('href')
+# # article_score=soup.find(name='span',class_='score')
+# # article_score_text=(article_score.getText())
+
